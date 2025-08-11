e4ddd7e25eb65b448ac9d680d0c710d736015ed2
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-08-12 05:56:41 +0630

    webTitleAdded

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href={{'img/bk999.png'}} />
     <title>BK999| Dashboard</title>
 
     <link rel="stylesheet"
